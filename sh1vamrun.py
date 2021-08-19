36bd305bd8768db6fa0720f6247bac50f08ff576
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-08-19 07:45:31 +0530

    Update sh1vamrun.py

--- sh1vamrun.py
+++ sh1vamrun.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/Sh1vam/javes-3.0 /root/ub && mkdir /root/ub/bin/ && cd /root/ub/ && mv ub/javes_main/extra/apktool /usr/local/bin && mv ub/javes_main/extra/apktool.jar /usr/local/bin && chmod +x /usr/local/bin/* && python3 -m ub")
+os.system("git clone https://github.com/Javes786/javes-2.0 /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && mv userbot/javes_main/extra/apktool /usr/local/bin && mv userbot/javes_main/extra/apktool.jar /usr/local/bin && chmod +x /usr/local/bin/* && python3 -m userbot")
